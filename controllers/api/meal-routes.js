cb3aabf577bbc3a6fd43bd078de4b5f59177b68d
Samantha Curatolo <85077075+scuratolo26@users.noreply.github.com>
2022-09-15 20:25:52 -0400

    get meals by id

--- controllers/api/meal-routes.js
+++ controllers/api/meal-routes.js
@@ -5,12 +5,33 @@ router.get('/', (req, res) => {
     // find all categories
     // be sure to include its associated Products
     Meal.findAll({
-        // include: [
-        //     {
-        //         model: Recipe,
-        //         attributes: ['id', 'title', 'meal_id']
-        //     }
-        // ]
+        include: [
+            {
+                model: Recipe,
+                attributes: ['id', 'title', 'recipe', 'meal_id']
+            }
+        ]
+    })
+        .then(dbMealData => res.json(dbMealData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+});
+
+router.get('/:id', (req, res) => {
+    // find all categories
+    // be sure to include its associated Products
+    Meal.findOne({
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Recipe,
+                attributes: ['id', 'title', 'recipe', 'meal_id']
+            }
+        ]
     })
         .then(dbMealData => res.json(dbMealData))
         .catch(err => {
