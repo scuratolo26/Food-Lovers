8b41201597c17e8da36fe8c735826d58375b2fbb
Samantha Curatolo <85077075+scuratolo26@users.noreply.github.com>
2022-09-22 12:21:57 -0400

    delete extra comments

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -40,26 +40,6 @@ router.get('/:id', (req, res) => {
     });
 });
 
-// CREATE new user
-// router.post('/', async (req, res) => {
-//   try {
-//     const dbUserData = await User.create({
-//       username: req.body.username,
-//       email: req.body.email,
-//       password: req.body.password,
-//     });
-
-//     req.session.save(() => {
-//       req.session.loggedIn = true;
-
-//       res.status(200).json(dbUserData);
-//     });
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json(err);
-//   }
-// });
-
 router.post('/', (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   User.create({
@@ -81,45 +61,6 @@ router.post('/', (req, res) => {
       res.status(500).json(err);
     });
 });
-
-
-// Login
-// router.post('/login', async (req, res) => {
-//   try {
-//     const dbUserData = await User.findOne({
-//       where: {
-//         email: req.body.email,
-//       },
-//     });
-
-//     if (!dbUserData) {
-//       res
-//         .status(400)
-//         .json({ message: 'Incorrect email or password. Please try again!' });
-//       return;
-//     }
-
-//     const validPassword = await dbUserData.checkPassword(req.body.password);
-
-//     if (!validPassword) {
-//       res
-//         .status(400)
-//         .json({ message: 'Incorrect email or password. Please try again!' });
-//       return;
-//     }
-
-//     req.session.save(() => {
-//       req.session.loggedIn = true;
-
-//       res
-//         .status(200)
-//         .json({ user: dbUserData, message: 'You are now logged in!' });
-//     });
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json(err);
-//   }
-// });
 
 router.post('/login', (req, res) => {
   // expects {email: 'lernantino@gmail.com', password: 'password1234'}
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -41,51 +41,52 @@ router.get('/', (req, res) => {
     });
 });
 
+// can use get single post for future development 
 // get single post
-router.get('/recipe/:id', (req, res) => {
-  Recipe.findOne({
-    where: {
-      id: req.params.id
-    },
-    attributes: [
-      'id',
-      'title',
-      'recipe'
-    ],
-    include: [
-      {
-        model: Meal,
-        attributes: [
-          'id',
-          'meal_name',
-        ],
-      },
-      {
-        model: User,
-        attributes: [
-          'username'
-        ]
-      }
-    ]
-  })
-    .then(dbRecipeData => {
-      if (!dbRecipeData) {
-        res.status(404).json({ message: 'No recipe found with this id' });
-        return;
-      }
+// router.get('/recipe/:id', (req, res) => {
+//   Recipe.findOne({
+//     where: {
+//       id: req.params.id
+//     },
+//     attributes: [
+//       'id',
+//       'title',
+//       'recipe'
+//     ],
+//     include: [
+//       {
+//         model: Meal,
+//         attributes: [
+//           'id',
+//           'meal_name',
+//         ],
+//       },
+//       {
+//         model: User,
+//         attributes: [
+//           'username'
+//         ]
+//       }
+//     ]
+//   })
+//     .then(dbRecipeData => {
+//       if (!dbRecipeData) {
+//         res.status(404).json({ message: 'No recipe found with this id' });
+//         return;
+//       }
 
-      const post = dbRecipeData.get({ plain: true });
+//       const post = dbRecipeData.get({ plain: true });
 
-      res.render('single-recipe', {
-        post,
-        loggedIn: req.session.loggedIn
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
+//       res.render('single-recipe', {
+//         post,
+//         loggedIn: req.session.loggedIn
+//       });
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
 
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
